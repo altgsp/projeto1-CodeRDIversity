f9f56e2094ff3cd35f8837002df19e29c2f21694
Geisa Santos <84372959+altgsp@users.noreply.github.com>
2024-08-24 23:47:18 -0300

    add: API Geladeira

---
+++ GeladeiraAPI/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuaAplicacao.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeladeiraController : ControllerBase
+    {
+        private readonly GeladeiraDbContext _context;
+
+        public GeladeiraController(GeladeiraDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdicionarItem(Item item)
+        {
+            _context.Itens.Add(item);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetItem", new { id = item.Id }, item);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetItens()
+        {
+            return await _context.Itens.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> GetItem(int id)
+        {
+            var item = await _context.Itens.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _context.Itens.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Itens.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Andar { get; set; }
+        public int Container { get; set; }
+        public int Posicao { get; set; }
+    }
+
+    public class GeladeiraDbContext : DbContext
+    {
+        public GeladeiraDbContext(DbContextOptions<GeladeiraDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Itens { get; set; }
+    }
+}
